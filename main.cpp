d8ebbc4cd4a77f778e288981959173eb44882312
Nicholas Manoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-04-07 14:15:37 -0700

    Def doesn't work but ok

--- main.cpp
+++ main.cpp
@@ -1,1 +1,162 @@
+#include <iostream>
+#include <fstream>
 
+enum Color { RED, BLACK };
+
+struct Node {
+    int data;
+    Color color;
+    Node* parent;
+    Node* left;
+    Node* right;
+
+    Node(int val) : data(val), color(RED), parent(nullptr), left(nullptr), right(nullptr) {}
+};
+
+class RedBlackTree {
+private:
+    Node* root;
+
+    void leftRotate(Node* x) {
+        Node* y = x->right;
+        x->right = y->left;
+        if (y->left != nullptr) {
+            y->left->parent = x;
+        }
+        y->parent = x->parent;
+        if (x->parent == nullptr) {
+            root = y;
+        } else if (x == x->parent->left) {
+            x->parent->left = y;
+        } else {
+            x->parent->right = y;
+        }
+        y->left = x;
+        x->parent = y;
+    }
+
+    void rightRotate(Node* x) {
+        Node* y = x->left;
+        x->left = y->right;
+        if (y->right != nullptr) {
+            y->right->parent = x;
+        }
+        y->parent = x->parent;
+        if (x->parent == nullptr) {
+            root = y;
+        } else if (x == x->parent->right) {
+            x->parent->right = y;
+        } else {
+            x->parent->left = y;
+        }
+        y->right = x;
+        x->parent = y;
+    }
+
+    void fixInsert(Node* z) {
+        while (z->parent != nullptr && z->parent->color == RED) {
+            if (z->parent == z->parent->parent->left) {
+                Node* y = z->parent->parent->right;
+                if (y != nullptr && y->color == RED) {
+                    z->parent->color = BLACK;
+                    y->color = BLACK;
+                    z->parent->parent->color = RED;
+                    z = z->parent->parent;
+                } else {
+                    if (z == z->parent->right) {
+                        z = z->parent;
+                        leftRotate(z);
+                    }
+                    z->parent->color = BLACK;
+                    z->parent->parent->color = RED;
+                    rightRotate(z->parent->parent);
+                }
+            } else {
+                Node* y = z->parent->parent->left;
+                if (y != nullptr && y->color == RED) {
+                    z->parent->color = BLACK;
+                    y->color = BLACK;
+                    z->parent->parent->color = RED;
+                    z = z->parent->parent;
+                } else {
+                    if (z == z->parent->left) {
+                        z = z->parent;
+                        rightRotate(z);
+                    }
+                    z->parent->color = BLACK;
+                    z->parent->parent->color = RED;
+                    leftRotate(z->parent->parent);
+                }
+            }
+        }
+        root->color = BLACK;
+    }
+
+    void insert(int data) {
+        Node* newNode = new Node(data);
+        Node* y = nullptr;
+        Node* x = root;
+
+        while (x != nullptr) {
+            y = x;
+            if (data < x->data) {
+                x = x->left;
+            } else {
+                x = x->right;
+            }
+        }
+
+        newNode->parent = y;
+        if (y == nullptr) {
+            root = newNode;
+        } else if (data < y->data) {
+            y->left = newNode;
+        } else {
+            y->right = newNode;
+        }
+
+        fixInsert(newNode);
+    }
+
+    void printTree(Node* node) {
+        if (node == nullptr) {
+            return;
+        }
+
+        printTree(node->left);
+        std::cout << "Number: " << node->data << ", Color: " << (node->color == RED ? "Red" : "Black");
+        if (node->parent != nullptr) {
+            std::cout << ", Parent: " << node->parent->data;
+        }
+        std::cout << std::endl;
+        printTree(node->right);
+    }
+
+public:
+    RedBlackTree() : root(nullptr) {}
+
+    void addNumber(int data) {
+        insert(data);
+    }
+
+    void readFromFile(const std::string& filename) {
+        std::ifstream file(filename);
+        int number;
+        while (file >> number) {
+            addNumber(number);
+        }
+        file.close();
+    }
+
+    void print() {
+        printTree(root);
+    }
+};
+
+int main() {
+    RedBlackTree tree;
+    tree.readFromFile("numbers.txt");
+    tree.print();
+
+    return 0;
+}
