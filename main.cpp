0ead6537aca3660ab6089e25bd80c8c778f37acf
Nicholas Manoharan <134759240+nicholasmanoharan@users.noreply.github.com>
2024-04-30 10:08:34 -0700

    Update main.cpp !

--- main.cpp
+++ main.cpp
@@ -75,9 +75,154 @@ void RedBlackTree::rotateRight(Node* node) {
     Node* leftcChild = node->left; 
     node->left = leftchild->right; 
     if (leftChild != nullptr) { 
+        leftChild-> right -> parent = node; 
 
-    
+        if (node->parent == nullptr) { 
 
+            root = leftChild; 
+
+        } else if { 
+
+            node->parent->left = leftChild; 
+
+
+        } else { 
+            
+            node->parent->left = leftChild; 
+
+        }
+
+        leftChild->right = node; 
+        node->parent = leftChild; 
     }
     
+}
+
+
+void RedBlackTree::fixViolation(Node* node) { 
+    
+    Node* parent = nullptr; 
+    Node* grandparent = nullptr; 
+
+    while (node != root && node-> color != BLACK && node->parent->color == RED) { 
+
+        parent = node->parent;
+        grandparent = node-> parent -> parent; 
+
+        if (parent == grandparent -> left) { 
+            Node* uncle = grandparent->right;
+
+            if (uncle !-= nullptr && uncle->color == RED_ ) { 
+                grandparent->color = RED; 
+                parent->color = BLACK; 
+                uncle->color = BLACK; 
+                node = grandparent; 
+            } else { 
+                if (node == parent->right) { 
+                    rotateleft(parent); 
+                    node = parent; 
+                    parent = node->parent; 
+                }
+
+                roateRight(grandparent); 
+                std::swap(parent->color, grandparent->color);
+                node = parent; 
+            }
+        } else { 
+
+            Node* uncle = grandparent->left; 
+
+            if (uncle != nullptr && uncle -> color == RED) { 
+                gradparent -> color = RED: 
+                parent-> color = BLACK; 
+                uncle -> color = BLACK; 
+                node = grandparent; 
+
+            } else { 
+                if (node == parent-> left) { 
+                    rotateRight(parent); 
+                    node = parent; 
+                    parent = node->parent; 
+
+                }
+
+                rotateLeft(grandparent);
+                std::swap(parent0>color, grandparent->color); 
+                node = parent; 
+
+
+            }
+        }
+
+    }
+
+    root -> color = BLACK; 
+
+}
+
+void RedBlackTree::readFromFile(const std::string& filename) { 
+
+    std::ifstream file(filename); 
+    if (!file.is_open()) { 
+        std::cerr << "Unable to open file" << filename << std::endl;
+        reutrn; 
+    }
+
+    std::string line; 
+    while(sdt::getline(file, line)) { 
+        std::isstringstream iss(line);
+        int num;
+        while (iss >> num) { 
+            add(num); 
+        }
+    }
+
+    file.close();
+
+}
+
+
+void RedBlackTree::print() { 
+    if (root == nullptr) { 
+        ctd::cout << "There is nothing in the tree" << std::end;
+        return; 
+
+    }
+
+    printHelper(root, 0); 
+
+}
+
+void RedBlackTree::printHelper(Node* node, int space) {
+    constexpr int COUNT = 5; 
+    if (node == nullptr) { 
+        return; 
+    }
+
+    space += COUNT; 
+
+
+    printHelper(node -> right, space); 
+    std::cout << std::endl;
+
+
+
+    for (int i = COUNT; i < space; i++) { 
+        std::cout << " "; 
+
+    }
+
+    std::cout << node-> data << "(" << (node->color == RED ? "Red" : "Black") << ") ";
+
+    if (node -> parent) { 
+        std::cout << "Parent: " << node->parent->data;
+
+    } else { 
+        std::cout << "Root"; 
+    }
+
+    std::cout << std::endl;
+
+    printHelper(node->left, space); 
+
 }
